1438501e7d091bcb1e4f3be6709a3fc26eecb7cf
Jason Clark <jason@clarkhome.us>
2012-05-20 13:51:59 -0500

    fixes

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,7 +14,7 @@ class lighttpd::params {
   $server_tag = 'lighttpd'
   $server_pidfile = '/var/run/lighttpd.pid'
   $server_errorlog = '/var/log/lighttpd/error.log'
-  $server_eventhandler = 'linux=sysepoll'
+  $server_eventhandler = 'linux-sysepoll'
   $server_networkbackend = 'linux-sendfile'
   $server_statcacheengine = 'simple'
   $server_maxconnections = '1024'
@@ -24,12 +24,19 @@ class lighttpd::params {
   $server_followsymlink = 'enable'
   $server_uploaddirs = '/var/tmp'
 
+  $server_username = $::operatingsystem ? {
+    /(?i-mx:debian|ubuntu)/                    => 'lighttpd',
+    /(?i-mx:fedora|rhel|centos|suse|opensuse)/ => 'lighttpd',
+  }
+  $server_groupname = $::operatingsystem ? {
+    /(?i-mx:debian|ubuntu)/                    => 'lighttpd',
+    /(?i-mx:fedora|rhel|centos|suse|opensuse)/ => 'lighttpd',
+  }
+
   case $::osfamily {
     'RedHat': {
       $config_file = '/etc/lighttpd/lighttpd.conf'
       $modules_file = '/etc/lighttpd/modules.conf'
-      $server_username = 'lighttpd'
-      $server_groupname = 'lighttpd'
     }
   }
 
